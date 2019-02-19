b89031893dd738ba76d42c4f9096709a864a62c6
prprhyt <prprhyt@gmail.com>
2019-02-19 23:36:31 +0900

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:twitter/twitter.dart';
+import 'package:oauth2/oauth2.dart';
+import 'dart:async';
+import 'dart:convert';
 
 void main() => runApp(MyApp());
+
+final requestTokenEndPoint =
+    Uri.parse("https://api.twitter.com/oauth/request_token");
+final accessTokenEndPoint =
+    Uri.parse("https://api.twitter.com/oauth/authorize");
+final redirectUrl = Uri.parse("http://localhost:8080/");
+final identifier = "my client identifier";
+final secret = "my client secret";
 
 class MyApp extends StatelessWidget {
   @override
@@ -40,8 +51,8 @@ class _BasePageState extends State<BasePage> {
             });
           },
           children: [
-            RandomWords(title: "page 1"), //call pages
-            RandomWords(title: "page 2"),
+            RandomWords2(title: "Notifications"), //call pages
+            RandomWords(title: "Direct Messages"),
           ]),
       bottomNavigationBar: BottomNavigationBar(
         onTap: (int index) { // define animation
@@ -111,4 +122,106 @@ class RandomWords extends StatefulWidget {
   final String title;
   @override
   RandomWordsState createState() => new RandomWordsState();
+}
+
+class Tweet {
+  String tweet="";
+  String username="";
+  Tweet(this.tweet,this.username);
+}
+
+class RandomWordsState2 extends State<RandomWords2> {
+  final _suggestions = <Tweet>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  int _counter = 0;
+  var length;
+  var page = 1;
+
+  Future<void> getTimeline() async {
+      Twitter twitter= new Twitter();
+      var response = await twitter.request("GET", "statuses/mentions_timeline.json?page=${page}");
+      List parsedList = jsonDecode(response.body);
+      twitter.close();
+      setState(() {
+        page += 1;
+        for(var i=0;i<parsedList.length;++i){
+          _suggestions.add(Tweet(parsedList[i]["text"],parsedList[i]["user"]["name"]));
+        }
+      });
+}
+
+  // #enddocregion RWS-var
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      getTimeline();
+    });
+  }
+
+  // #docregion _buildSuggestions
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
+
+          //final index = i ~/ 2; /*3*/
+          if (i == length) {
+            //_suggestions.addAll(getTimeline()); /*4*/
+            getTimeline();
+            // 画面にはローディング表示しておく
+            return new Center(
+              child: new Container(
+                margin: const EdgeInsets.only(top: 8.0),
+                width: 32.0,
+                height: 32.0,
+                child: const CircularProgressIndicator(),
+              ),
+            );
+          }else if(i > length){
+            return null;
+          }
+          return _buildRow(_suggestions[i]);
+        });
+  }
+  // #enddocregion _buildSuggestions
+
+  // #docregion _buildRow
+  Widget _buildRow(Tweet pair) {
+    return ListTile(
+      title: Text(
+        pair.tweet,
+        style: _biggerFont,
+      ),
+    );
+  }
+  // #enddocregion _buildRow
+
+  // #docregion RWS-build
+  @override
+  Widget build(BuildContext context) {
+    length = _suggestions?.length ?? 0;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: _buildSuggestions(),
+            floatingActionButton: FloatingActionButton(
+              onPressed: _incrementCounter,
+              tooltip: 'Increment',
+              child: Icon(Icons.add),
+            ),
+    );
+  }
+  // #enddocregion RWS-build
+  // #docregion RWS-var
+}
+// #enddocregion RWS-var
+
+class RandomWords2 extends StatefulWidget {
+  final String title;
+  RandomWords2({Key key, this.title}) : super(key: key);
+
+  @override
+  RandomWordsState2 createState() => new RandomWordsState2();
 }
